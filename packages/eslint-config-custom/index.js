0afbf28ca758df4a97ac5767a4fcd87cf16f80d3
Thanayut T <b@thanayut.in.th>
2023-01-01 22:03:57 +0700

    feat: add eslint simple-import-sort plugin

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,4 +1,8 @@
 module.exports = {
   extends: ['turbo', 'prettier'],
-  rules: {},
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 };
