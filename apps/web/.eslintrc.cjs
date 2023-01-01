b622df81a97b57fc266df17d34e7a75b55c23557
Thanayut T <b@thanayut.in.th>
2023-01-01 18:06:07 +0700

    feat: add web

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ["airbnb", "airbnb-typescript", "custom"],
+  parserOptions: {
+    project: ["./apps/web/tsconfig.json"],
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
